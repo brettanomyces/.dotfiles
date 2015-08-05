b49aae50b041a580ff9c04a0033232e6006a84d2
yukichb <brett.yukich@fostermoore.co.nz>
2015-08-06 08:15:36 +1200

    include local application

--- fa.fish
+++ fa.fish
@@ -1,7 +1,10 @@
 function fa
    set tmpFile /tmp/fa.tmp
-   find /usr/share/applications/ -regex '.*.desktop' | fzf > $tmpFile; \
-     and exo-open (cat $tmpFile) >/dev/null ^/dev/null
+   begin
+     find /usr/share/applications/ -regex '.*.desktop'
+     find /usr/local/share/applications/ -regex '.*.desktop'
+   end | fzf > $tmpFile 
+   and exo-open (cat $tmpFile) ^/dev/null
    rm $tmpFile
 end
 
