8d598946ee3dbccd311e23516254e230e9fe9088
yukichb <brett.yukich@fostermoore.co.nz>
2015-08-06 09:57:18 +1200

    avoid calling fkill if we cancel command

--- fkill.fish
+++ fkill.fish
@@ -1,3 +1,8 @@
 function fkill
-  ps h -eo pid,comm,cmd | fzf --extended-exact | awk '{print $1}' | xargs kill $argv
+  set tmpFile /tmp/fkill.tmp
+  ps h -eo pid,comm,cmd | fzf --extended-exact | awk '{print $1}' > $tmpFile
+  if test -s $tmpFile
+    kill $argv (cat $tmpFile)
+  end
+  rm $tmpFile
 end
