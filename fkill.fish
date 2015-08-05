735c2bdbdbce63d600e379f4f52a27974856188f
yukichb <brett.yukich@fostermoore.co.nz>
2015-08-05 15:57:45 +1200

    dont include headers in ps output

--- fkill.fish
+++ fkill.fish
@@ -1,3 +1,3 @@
 function fkill
-  ps -eo pid,comm,cmd | fzf --extended-exact | awk '{print $1}' | xargs kill $argv
+  ps h -eo pid,comm,cmd | fzf --extended-exact | awk '{print $1}' | xargs kill $argv
 end
