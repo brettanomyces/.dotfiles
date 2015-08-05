c87ec5b2964fd0cb68ebcefbf5a93fb7cdb6dc33
yukichb <brett.yukich@fostermoore.co.nz>
2015-08-05 15:57:11 +1200

    use unique tmp file and remove after use

--- fd.fish
+++ fd.fish
@@ -1,11 +1,12 @@
 function fd
   if count $argv > /dev/null
     # fish arrays start at 1
-    set fromDir $argv[1]
+    set dir $argv[1]
   else
-    set fromDir '.'
+    set dir '.'
   end
-  set toDir /tmp/dir
-  find $fromDir -regex '.*/\.\(git\|svn\).*' -prune -o \
-    -maxdepth 5 -type d -print 2> /dev/null | fzf > $toDir; and cd (cat $toDir)
+  set tmpFile /tmp/fd.tmp
+  find $dir -regex '.*/\.\(git\|svn\).*' -prune -o \
+    -maxdepth 5 -type d -print 2> /dev/null | fzf > $tmpFile; and cd (cat $tmpFile)
+  rm $tmpFile
 end
