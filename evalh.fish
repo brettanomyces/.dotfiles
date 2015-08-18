afe497dc696da66c11831f0bf74bebc01b83c98d
yukichb <brett.yukich@fostermoore.co.nz>
2015-08-18 15:12:54 +1200

    make evalh'd command appear as the previous command in history

--- evalh.fish
+++ evalh.fish
@@ -1,6 +1,19 @@
 function evalh
-	echo "- cmd:" $argv >> ~/.config/fish/fish_history
-	echo "  when:" (date "+%s") >> ~/.config/fish/fish_history
-	history --merge
-	eval $argv
+    eval $argv
+
+    # backup history file
+    cp ~/.config/fish/fish_history /tmp/fish_history.tmp
+
+    # clear internal history and history file (using builtin means we don't get a prompt)
+    builtin history --clear
+
+    # restore history file
+    cp /tmp/fish_history.tmp ~/.config/fish/fish_history
+
+    # append our command 
+    echo "- cmd:" $argv >> ~/.config/fish/fish_history
+    echo "  when:" (date "+%s") >> ~/.config/fish/fish_history
+
+    # merge history file with (empty) internal history
+    history --merge
 end
