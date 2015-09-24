b282b119da7c875ef7ca1cabf8b7501c67e5d21d
yukichb <brett.yukich@fostermoore.co.nz>
2015-09-25 09:28:47 +1200

    use vi to edit commands

--- edit_command.fish
+++ edit_command.fish
@@ -1,7 +1,7 @@
 function edit_command
   set tmpFile /tmp/edit_command.tmp
   commandline > $tmpFile
-  eval $EDITOR $tmpFile
+  evalh /usr/bin/vi $tmpFile
   commandline -r (cat $tmpFile)
   rm $tmpFile
 end
