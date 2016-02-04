fcf00cfa27cc5c62fae98b5bf9dc8c7f6a28af5b
brettanomyces <brettyukich@gmail.com>
2016-02-04 16:40:32 +1300

    use Neovim as $EDITOR

--- config.fish
+++ config.fish
@@ -2,8 +2,8 @@
 set -gx fish_greeting ''
 
 # use Neovim as default editor
-#set -gx EDITOR /usr/local/bin/nvim
-set -gx EDITOR /usr/bin/vi
+set -gx EDITOR /usr/local/bin/nvim
+#set -gx EDITOR /usr/bin/vi
 
 # use Java 7 as default java
 set -gx JAVA_HOME /usr/lib/jvm/java-7-oracle
